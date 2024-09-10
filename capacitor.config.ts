41c558f0874ee074f2bbeda110eb8b32e3e7dd40
tanapat muijeen <tanapat.muijeen@gmail.com>
2024-09-10 14:07:26 +0700

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Project01-23',
+  appName: 'KruaPaLai',
   webDir: 'www'
 };
 
